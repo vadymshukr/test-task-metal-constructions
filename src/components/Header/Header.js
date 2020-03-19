c0efbdd862a1978db237807b14f8e198544d5b3f
vadymshukr <vadymshukr@gmail.com>
2020-03-19 20:05:50 +0200

    img folder created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,25 +6,33 @@ function Header() {
   return (
     <header className="header">
         <div className="container">
-            <nav className="menu">
-                <ul className="menu__list">
-                    <li className="menu__item">
-                        <a href="#" className="menu__link">Главная</a>
-                    </li>
-                    <li className="menu__item">
-                        <a href="#" className="menu__link">Услуги</a>
-                    </li>
-                    <li className="menu__item">
-                        <a href="#" className="menu__link">Наши работы</a>
-                    </li>
-                    <li className="menu__item">
-                        <a href="#" className="menu__link">О нас</a>
-                    </li>
-                    <li className="menu__item">
-                        <a href="#" className="menu__link">Контакты</a>
-                    </li>
-                </ul>
-            </nav>
+            <div className="header-wrapper">
+                <div className="logo">
+                    <a href="#" className="logo__link">
+                        <img src="img/icons/logo.png" alt="" className="logo__image"/>
+                    </a>
+                </div>
+                <nav className="menu">
+                    <ul className="menu__list">
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">Главная</a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">Услуги</a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">Наши работы</a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">О нас</a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">Контакты</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            
         </div>
     </header>
   );
