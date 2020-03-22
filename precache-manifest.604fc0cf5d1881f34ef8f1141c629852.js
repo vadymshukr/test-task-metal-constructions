97c4cc62faf9975b58753c1f6f964e8dcb853fcb
vadymshukr <vadymshukr@gmail.com>
2020-03-22 23:56:13 +0200

    Updates

---
+++ precache-manifest.604fc0cf5d1881f34ef8f1141c629852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d9f531594f1256a7b026007f3b48a2",
+    "url": "/test-task-metal-constructions/index.html"
+  },
+  {
+    "revision": "c81fb31dcdc64d1d5c24",
+    "url": "/test-task-metal-constructions/static/css/main.720192a6.chunk.css"
+  },
+  {
+    "revision": "79382205c294fd593f21",
+    "url": "/test-task-metal-constructions/static/js/2.f1be127d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test-task-metal-constructions/static/js/2.f1be127d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81fb31dcdc64d1d5c24",
+    "url": "/test-task-metal-constructions/static/js/main.edaacd52.chunk.js"
+  },
+  {
+    "revision": "382894dfdea6a5db1c42",
+    "url": "/test-task-metal-constructions/static/js/runtime-main.d387b4b3.js"
+  }
+]);
